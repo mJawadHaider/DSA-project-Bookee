63d3c9c9bbfd1b696ee2c04a9edb9b5f0ee27092
danishgondaldev <danishakramgondal@gmail.com>
2022-12-15 17:53:29 +0500

    added headings in sign in and sign up form

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <v-app>
     <v-main>
-      <HomePage  />
+      <HomePage />
     </v-main>
   </v-app>
 </template>
--- frontend/src/components/HomePage.vue
+++ frontend/src/components/HomePage.vue
@@ -9,51 +9,53 @@
         <v-col cols="12" md="12">
           <h3 class="font-italic">
             Welcome to Bookee. Let's read, as reading heals!
-          </h3> 
+          </h3>
         </v-col>
-          <!-- <v-btn rounded color="brown" dark elevation="10" class="font-weight-black">
-            Click Here To Sign Up
-          </v-btn> -->
       </v-row>
     </v-row>
     <v-row class="mt-8 justify-center">
-      <v-card  width="35%" color="#EFEBE9" elevation="7">
-        <v-col cols="12" md="12" sm="12" class="pt-3 white">
-          <v-text-field
-            v-model="user.email"
-            placeholder="Enter Email"
-            :required=[true]
-          />
-        </v-col>
-        <v-col cols="12" md="12" sm="12" class="py-0">
-          <v-text-field
-            v-model="user.password"
-            placeholder="Enter Password"
-            :required=[true]
-          />
-        </v-col>
-        <v-col cols="12" md="12" sm="12" class="pa-0 text-end">
-          <u class="ma-3">Forgot Password? Click Here</u>
-        </v-col>
-        <v-col cols="12" md="12" sm="12" class="text-end">
-          <v-btn color="#DABDAB">Sign In</v-btn>
-        </v-col>
-        <v-row class="d-flex align-center">
-          <v-col class="text-end" cols="6">Did not register yet?</v-col>
-          <v-col class="mb-1 pl-0" cols="6">
-            <v-btn
-              variant="outlined"
-              color="brown"
-              class="ml-0"
-            >
-              Register Now
-            </v-btn>
+      <v-card width="35%" color="#EFEBE9" elevation="7">
+        <v-form ref="signInForm" valid="validForm">
+          <v-row class="d-flex align-center">
+            <v-col class="text-center pt-6" cols="12">
+            <h3 class="font-italic" style="background-color:saddlebrown; color:white;">
+             SIGN IN
+            </h3>
+            </v-col>
+          </v-row>
+          <v-col cols="12" md="12" sm="12" class="pt-3 white">
+            <v-text-field v-model="user.email" placeholder="Enter Email" variant="outlined" label="Email"
+              :rules="[rules.required, rules.email]" required />
           </v-col>
-        </v-row>
-      </v-card >
+          <v-col cols="12" md="12" sm="12" class="py-0">
+            <v-text-field v-model="user.password" placeholder="Enter Password" variant="outlined" label="Password"
+              :append-inner-icon="visible ? 'mdi-eye-off' : 'mdi-eye'" :type="visible ? 'text' : 'password'"
+              @click:append-inner="visible = !visible" :rules="[rules.required, rules.maxlength]" required />
+          </v-col>
+          <v-col cols="12" md="12" sm="12" class="pa-0 text-end">
+            <u class="ma-3">Forgot Password? Click Here</u>
+          </v-col>
+          <v-col cols="12" md="12" sm="12" class="text-end">
+            <v-btn color="#DABDAB" @click="onClick">Sign In</v-btn>
+          </v-col>
+          <v-row class="d-flex align-center">
+            <v-col class="text-end" cols="6">Did not register yet?</v-col>
+            <v-col class="mb-1 pl-0" cols="6">
+              <v-btn variant="outlined" color="brown" class="ml-0" size="small" @click="dialog = true">
+                Register Now
+              </v-btn>
+            </v-col>
+          </v-row>
+        </v-form>
+      </v-card>
     </v-row>
   </div>
-  
+  <v-dialog v-model="dialog" content-class="align-center">
+    <v-card max-width="400px">
+      <modal-sign-up />
+    </v-card>
+  </v-dialog>
+
   <v-footer>
     <v-card width="100%" color="brown" height="40px" class="d-flex align-center justify-center">
       &copy; 2022 Bookee - All Rights Reserved
@@ -61,19 +63,35 @@
   </v-footer>
 </template>
 <script>
+import ModalSignUp from './ModalSignUp.vue';
 
 export default {
   name: 'Home-Page',
 
   components: {
+    ModalSignUp
   },
 
   data: () => ({
+    dialog: false,
     user: {
-      email: 'admin@gmail.com',
-      password: 'admin123',
-    }
+      email: '',
+      password: '',
+    },
+    rules: {
+      required: (a) => !!a || 'This field is required',
+      maxlength: (a) => a.length >= 8 || 'Minimum 8 characters are required',
+      email: (v) => /.+@.+\..+/.test(v) || 'E-mail must be valid',
+    },
   }),
+  methods: {
+    async onClick() {
+      const { valid } = await this.$refs.signInForm.validate();
+      if (valid) {
+        alert('Form is valid');
+      }
+    }
+  }
 }
 </script>
 <style scoped>
@@ -81,5 +99,10 @@ export default {
   background: url('../assets/background_new.svg');
   background-size: 100%;
   height: 100%;
+}
+
+.dialog {
+  align-items: center;
+  color: blue;
 }
 </style>
